d32a3758f69fa358958a4651c4aa6b3bd270ae25
Lacey Avalon <laceyavalon@GCCYJTSrSchools.attlocal.net>
2019-10-03 23:27:52 -0700

    adds initial version of Hero Split

--- src/blocks/hero-split/hero-split.js
+++ src/blocks/hero-split/hero-split.js
@@ -1,28 +1,228 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
+const { TextControl, ToggleControl, SelectControl } = wp.components;
+const { RichText, InspectorControls } = wp.editor;
 
-registerBlockType('ehi-pillar-blocks/hero-split', {
-    title: __('Hero Split'),
-    icon: 'smiley',
-    category: 'widgets',
+const validAlignments = ["full"];
 
-    edit() {
-        return (
-            <div>
-                <hero-split
-                    title="is this working?"
-                    subtitle="how about this"
-                >
-
-                </hero-split>
-            </div>
-        )
+registerBlockType("ehi-pillar-blocks/hero-split", {
+    title: __("Hero Split"),
+    icon: "align-right",
+    category: "widgets",
+    attributes: {
+        align: {
+            type: "string",
+            default: "full"
+        },
+        headline: {
+            type: "string",
+            default: "Compare and find affordable health insurance."
+        },
+        subtitle: {
+            type: "string",
+            default:
+                "Over 4 million customers have found a health plan through our marketplace."
+        },
+        ctaType: {
+            type: "string",
+            default: "button"
+        },
+        ctaText: {
+            type: "string",
+            default: "Find Health Plans"
+        },
+        imgSrc: {
+            type: "string",
+            default:
+                "https://ik.imagekit.io/ehealth/ifp-dad__cropped__1__V8A-d_PQm.png"
+        },
+        color: {
+            type: "string",
+            default: "green"
+        },
+        featureContent: {
+            type: "string",
+            default: "equal"
+        },
+        isReverse: {
+            type: "boolean",
+            default: false
+        },
+        useBottomDivider: {
+            type: "boolean",
+            default: false
+        },
+        bottomDividerColor: {
+            type: "string",
+            default: "blue"
+        },
+        blockSpacing: {
+            type: "string",
+            default: ""
+        }
     },
-    save() {
+
+    getEditWrapperProps(attributes) {
+        const { align } = attributes;
+        if (-1 !== validAlignments.indexOf(align)) {
+            return { "data-align": align };
+        }
+    },
+
+    edit({ attributes, setAttributes }) {
+        const {
+            headline,
+            subtitle,
+            ctaType,
+            ctaText,
+            imgSrc,
+            color,
+            featureContent,
+            isReverse,
+            useBottomDivider,
+            bottomDividerColor,
+            blockSpacing
+        } = attributes;
+
+        return [
+            <InspectorControls>
+                <TextControl
+                    label="Headline"
+                    value={headline}
+                    onChange={newValue => setAttributes({ headline: newValue })}
+                />
+                <TextControl
+                    label="Subtitle"
+                    value={subtitle}
+                    onChange={newValue => setAttributes({ subtitle: newValue })}
+                />
+                <SelectControl
+                    label="ctaType"
+                    value={ctaType}
+                    onChange={newValue => setAttributes({ ctaType: newValue })}
+                    options={[
+                        { value: "button", label: "Button" },
+                        { value: "email", label: "Email" }
+                    ]}
+                />
+                <TextControl
+                    label="ctaText"
+                    value={ctaText}
+                    onChange={newValue => setAttributes({ ctaText: newValue })}
+                />
+                <TextControl
+                    label="imgSrc"
+                    value={imgSrc}
+                    onChange={newValue => setAttributes({ imgSrc: newValue })}
+                />
+                <SelectControl
+                    label="color"
+                    value={color}
+                    onChange={newValue => setAttributes({ color: newValue })}
+                    options={[
+                        { value: "white", label: "White" },
+                        { value: "red", label: "Red" },
+                        { value: "blue", label: "Blue" },
+                        { value: "orange", label: "Orange" },
+                        { value: "green", label: "Green" },
+                        { value: "gray", label: "Gray" }
+                    ]}
+                />
+                <SelectControl
+                    label="featureContent"
+                    value={featureContent}
+                    onChange={newValue =>
+                        setAttributes({ featureContent: newValue })
+                    }
+                    options={[
+                        { value: "image", label: "Image" },
+                        { value: "content", label: "Content" },
+                        { value: "equal", label: "Equal" }
+                    ]}
+                />
+                <ToggleControl
+                    label="Reverse the layout?"
+                    checked={isReverse}
+                    onChange={newValue =>
+                        setAttributes({ isReverse: newValue })
+                    }
+                />
+                <ToggleControl
+                    label="Add a bottom divider?"
+                    checked={useBottomDivider}
+                    onChange={newValue =>
+                        setAttributes({ useBottomDivider: newValue })
+                    }
+                />
+                {useBottomDivider && (
+                    <SelectControl
+                        label="bottomDividerColor"
+                        value={bottomDividerColor}
+                        onChange={newValue =>
+                            setAttributes({ bottomDividerColor: newValue })
+                        }
+                        options={[
+                            { value: "white", label: "White" },
+                            { value: "red", label: "Red" },
+                            { value: "blue", label: "Blue" },
+                            { value: "orange", label: "Orange" },
+                            { value: "green", label: "Green" },
+                            { value: "gray", label: "Gray" }
+                        ]}
+                    />
+                )}
+                <TextControl
+                    label="Wrapper CSS classes"
+                    value={blockSpacing}
+                    onChange={newValue =>
+                        setAttributes({ blockSpacing: newValue })
+                    }
+                />
+            </InspectorControls>,
+            <hero-split
+                headline={headline}
+                subtitle={subtitle}
+                cta-type={ctaType}
+                cta-text={ctaText}
+                img-src={imgSrc}
+                color={color}
+                feature-content={featureContent}
+                is-reverse={isReverse}
+                use-bottom-divider={useBottomDivider}
+                bottom-divider-color={bottomDividerColor}
+                block-spacing={blockSpacing}
+            ></hero-split>
+        ];
+    },
+    save({ attributes }) {
+        const {
+            headline,
+            subtitle,
+            ctaType,
+            ctaText,
+            imgSrc,
+            color,
+            featureContent,
+            isReverse,
+            useBottomDivider,
+            bottomDividerColor,
+            blockSpacing
+        } = attributes;
+
         return (
-            <div>
-                <h2>This is a simple block (front end)</h2>
-            </div>
-        )
+            <hero-split
+                headline={headline}
+                subtitle={subtitle}
+                cta-type={ctaType}
+                cta-text={ctaText}
+                img-src={imgSrc}
+                color={color}
+                feature-content={featureContent}
+                is-reverse={isReverse}
+                use-bottom-divider={useBottomDivider}
+                bottom-divider-color={bottomDividerColor}
+                block-spacing={blockSpacing}
+            ></hero-split>
+        );
     }
 });
